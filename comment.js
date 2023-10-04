6a834d48904d646cf671dc5629801aa735d53967
Christos O. Andrikos <mcchran@gmail.com>
2023-10-04 11:35:09 +0300

    feat: formatted comment output

--- comment.js
+++ comment.js
@@ -8,11 +8,41 @@ function getInputs() {
     };
 }
 function getState() { 
-    const failed = core.getState("failed");
+    const failed = JSON.parse(core.getState("failed"));
     return {
         failed
     }; 
 }
+
+// a function to pad a string with spaces to a given length
+function padString(string, length) {
+    return string + " ".repeat(length - string.length);
+}
+
+function formatComment(failed) {
+    let comment = `The following image-tags are not available on dockerhub: \n\n`;
+    let maxRepoTagLength = 0;
+    let maxReasonLength = 0;
+
+    // Iterate through the failed to find the maximum lengths
+    failed.forEach(item => {
+    if (item.repoTag.length > maxRepoTagLength) {
+        maxRepoTagLength = item.repoTag.length;
+    }
+
+    if (item.reason.length > maxReasonLength) {
+        maxReasonLength = item.reason.length;
+    }
+    });
+
+
+    comment += `|${padString("repoTag", maxRepoTagLength)} | ${padString("Reason", maxReasonLength)} |\n`;
+    failed.forEach(item => {
+        comment += `|${padString(item.repoTag, maxRepoTagLength)} | ${padString(item.reason, maxReasonLength)} |` + "\n";
+    });
+
+    return comment;
+  }
 
 async function run() {
     const {
@@ -27,24 +57,30 @@ async function run() {
     const { pull_request } = github.context.payload;
     const { owner, repo } = github.context.repo;
 
-    // console.log("pull_request: ", pull_request);
-    // console.log("owner: ", owner);
-    // console.log("repo: ", repo);
-    console.log(`failed: ${failed}`);
-
     // let's add a comment to the exracted PR with the results of the check.
-    console.log("the toekn:", githubToken)
     const octokit = github.getOctokit(githubToken);
-    const comment = `Hello from the check_image_tag action! This is a test comment. \n\n failed: ${failed}`;
-    const issue_number = pull_request.number;
-    console.log(`issue_number: ${issue_number}`);
+
+    if (failed.length === 0) {
+        const _ = await octokit.rest.issues.createComment({
+            owner,
+            repo,
+            issue_number,
+            body: "No failed images found!",
+        });
+        process.exit(0);
+    }
     
+    const comment = formatComment(failed);
+
+    const issue_number = pull_request.number;
     const _ = await octokit.rest.issues.createComment({
         owner,
         repo,
         issue_number,
         body: comment,
     });
+
+
 }
 
 run();
